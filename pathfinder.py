b15c68323314390f8a163734b9e1a3ba288a22b3
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2023-03-21 15:41:07 +1030

    added test inputs

--- pathfinder.py
+++ pathfinder.py
@@ -1,10 +1,24 @@
 import numpy as np
+search = "bfs"
+size = [10,10]
+start = [1,1]
+end = [10,10]
+inpMap = np.array([1, 1, 1, 1, 1, 1, 4, 7, 8, "X"],
+                  [1, 1, 1, 1, 1, 1, 1, 5, 8, 8],
+                  [1, 1, 1, 1, 1, 1, 1, 4, 6, 7],
+                  [1, 1, 1, 1, 1, "X", 1, 1, 1, 6],
+                  [1, 1, 1, 1, 1, "X", 1, 1, 1, 1],
+                  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                  [6, 1, 1, 1, 1, "X", 1, 1, 1, 1],
+                  [7, 7, 1, "X", "X", "X", 1, 1, 1, 1],
+                  [8, 8, 1, 1, 1, 1, 1, 1, 1, 1],
+                  ["X", 8, 7, 1, 1, 1, 1, 1, 1, 1])
 
 def GraphSearch(problem, fringe):
     closed = {}
     fringe = Insert(MakeNode(initialState[problem]), fringe)
     loopCount = 0
-    
+
     while loopCount < 10000:
         loopCount += 1
         if len(fringe) == 0:
