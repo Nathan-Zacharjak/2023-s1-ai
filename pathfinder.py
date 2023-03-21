60729184a666f34cfa6d808d1f51e886279c1902
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2023-03-21 16:24:39 +1030

    begun createFringe()

--- pathfinder.py
+++ pathfinder.py
@@ -13,24 +13,43 @@ inpMap = np.array([1, 1, 1, 1, 1, 1, 4, 7, 8, "X"],
                   [7, 7, 1, "X", "X", "X", 1, 1, 1, 1],
                   [8, 8, 1, 1, 1, 1, 1, 1, 1, 1],
                   ["X", 8, 7, 1, 1, 1, 1, 1, 1, 1])
+def CreateFringe(search, node, closed, inpMap):
+    fringe = []
+    if node in closed:
+        return fringe
+    x = node[0]
+    y = node[1]
 
-def GraphSearch(problem, fringe):
+    up = inpMap[x,y-1]
+    down = inpMap[x,y+1]
+    left = inpMap[x-1,y]
+    right = inpMap[x+1,y]
+    potentialFringe = [up,down,left,right]
+    print(potentialFringe)
+    return fringe
+
+def GraphSearch(search, size, start, end, inpMap):
+    outMap = inpMap
     closed = {}
-    fringe = Insert(MakeNode(initialState[problem]), fringe)
+    fringe = CreateFringe(search, start, closed, inpMap)
     loopCount = 0
 
-    while loopCount < 10000:
-        loopCount += 1
-        if len(fringe) == 0:
-            return False
+    # while loopCount < 10000:
+    #     loopCount += 1
+    #     if len(fringe) == 0:
+    #         return False
         
-        node = RemoveFront(fringe)
+        # node = RemoveFront(fringe)
 
-        if GoalTest(problem, state[node]):
-            return node
+        # if GoalTest(problem, state[node]):
+        #     return outMap
         
-        if not (state[node] in closed):
-            closed.add(state[node])
-            fringe = InsertAll(Expand(node, problem), fringe)
+        # if not (state[node] in closed):
+        #     closed.add(state[node])
+        #     fringe = InsertAll(Expand(node, problem), fringe)
 
-# GraphSearch()
+result = GraphSearch(search, size, start, end, inpMap)
+if not result:
+    print("null")
+else:
+    print(result)
