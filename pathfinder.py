71215a523a993e7344e3dc7cb25bc0634ed57f6a
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2023-03-21 15:22:35 +1030

    added sudocode

--- pathfinder.py
+++ pathfinder.py
@@ -1,5 +1,22 @@
 import numpy as np
 
-rng = np.random.default_rng()
-numbs = rng.normal(size=5)
-print(numbs)
+def GraphSearch(problem, fringe):
+    closed = {}
+    fringe = Insert(MakeNode(initialState[problem]), fringe)
+    loopCount = 0
+    
+    while loopCount < 10000:
+        loopCount += 1
+        if len(fringe) == 0:
+            return False
+        
+        node = RemoveFront(fringe)
+
+        if GoalTest(problem, state[node]):
+            return node
+        
+        if not (state[node] in closed):
+            closed.add(state[node])
+            fringe = InsertAll(Expand(node, problem), fringe)
+
+# GraphSearch()
