b450dbeda3aec15b359701458e1af5b8dceb4e4d
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2023-03-24 12:52:25 +1030

    Cleaned up a lot but still not working...

--- pathfinder.py
+++ pathfinder.py
@@ -21,25 +21,22 @@ inpMap = [[1, 1, "X"],
 #         [8, 8, 1, 1, 1, 1, 1, 1, 1, 1],
 #         ["X", 8, 7, 1, 1, 1, 1, 1, 1, 1]]
 
-def GeneratePath(map, closed, start, end):
+def GeneratePath(map, start, consideredNode):
+    currentNode = consideredNode
     loopCount = 0
-    currentNode = end
 
     while loopCount < 10000:
         loopCount += 1
 
-        # Current node stuff
+        # Staring the current node on the map
         row = currentNode[0]
         col = currentNode[1]
         map[row][col] = "*"
         if (row == start[0]) and (col == start[1]):
             return map
 
-        # Parent node stuff
-        parentNode = closed[(row, col)]
-        parentRow = parentNode[0]
-        parentCol = parentNode[1]
-        currentNode = (parentRow, parentCol)
+        # Getting the parent node of the current node
+        currentNode = currentNode[2]
 
     if loopCount == 10000:
         return "Generate path loop count limit reached!"
@@ -48,13 +45,13 @@ def GeneratePath(map, closed, start, end):
 
 # Returns true if the current node is the end node and
 # sets off the generation of the path
-def CheckIfEndNode(consideredNode, start, end, map, closed):
+def CheckIfEndNode(consideredNode, start, end, map):
     rowPos = consideredNode[0]
     colPos = consideredNode[1]
     print("is consideredNode end:", consideredNode, end, rowPos == end[0] and colPos == end[1])
 
     if (rowPos == end[0]) and (colPos == end[1]):
-        outMap = GeneratePath(map, closed, start, end)
+        outMap = GeneratePath(map, start, consideredNode)
 
         return outMap, True
     else:
@@ -68,42 +65,45 @@ def ExpandFringe(closed, size, map, fringe, consideredNode, fringeIndex):
     col = consideredNode[1]
     consideredNodeDepth = consideredNode[3]
         
-    up = (row-1,col,(row,col,consideredNode[4]),consideredNodeDepth + 1, "up", fringeIndex)
+    up = (row-1,col,consideredNode,consideredNodeDepth + 1, "up", fringeIndex)
     fringeIndex += 1
-    down = (row+1,col,(row,col,consideredNode[4]),consideredNodeDepth + 1, "down", fringeIndex)
+    down = (row+1,col,consideredNode,consideredNodeDepth + 1, "down", fringeIndex)
     fringeIndex += 1
-    left = (row,col-1,(row,col,consideredNode[4]),consideredNodeDepth + 1, "left", fringeIndex)
+    left = (row,col-1,consideredNode,consideredNodeDepth + 1, "left", fringeIndex)
     fringeIndex += 1
-    right = (row,col+1,(row,col,consideredNode[4]),consideredNodeDepth + 1, "right", fringeIndex)
+    right = (row,col+1,consideredNode,consideredNodeDepth + 1, "right", fringeIndex)
     fringeIndex += 1
     potentialExpand = [up,down,left,right]
 
-    print("Considered node:", consideredNode)
-    print("Potential expand:", potentialExpand)
+    # print("Considered node:", consideredNode)
+    # print("Potential expand:", potentialExpand)
     for node in potentialExpand:
         rowPos = node[0]
         colPos = node[1]
 
-        if rowPos < 0 or rowPos > size[0] - 1:
+        # print("Node:", node)
+        # print("RowPos:", rowPos < 0, rowPos > size[0] - 1)
+        # print("ColPos:", colPos < 0, colPos > size[0] - 1)
+        if (rowPos < 0) or (rowPos > size[0] - 1):
             continue
-        if colPos < 0 or colPos > size[0] - 1:
+        if (colPos < 0) or (colPos > size[0] - 1):
             continue
         if map[rowPos][colPos] == "X":
             continue
-        if (rowPos, colPos) in closed:
-            continue
+        # if (rowPos, colPos) in closed:
+        #     continue
 
-        foundDupeNode = False
-        for fringeNode in fringe:
-            if (fringeNode[0] == rowPos) and (fringeNode[1] == colPos) and (fringeNode[2] == (row, col)):
-                foundDupeNode = True
-                break
-        if foundDupeNode:
-            continue
+        # foundDupeNode = False
+        # for fringeNode in fringe:
+        #     if (fringeNode[0] == rowPos) and (fringeNode[1] == colPos) and (fringeNode[2] == (row, col)):
+        #         foundDupeNode = True
+        #         break
+        # if foundDupeNode:
+        #     continue
 
         fringe.append(node)
 
-    print("Fringe:", fringe)
+    # print("Fringe:", fringe)
     return fringe, fringeIndex
 
 # Returns the next node to expand based on the fringe
@@ -127,6 +127,8 @@ def ChooseNextConsideredNode(fringe, map):
         depth = node[3]
         if depth == minDepth:
             nextNodes.append(node)
+    
+    # print("optimal nodes:", nextNodes)
 
     # Now from all these equally optimal nodes,
     # apply the "up, down, left right" priority to resolve a tie,
@@ -180,22 +182,27 @@ def ChooseNextConsideredNode(fringe, map):
     # elif len(rightNodes) > 0:
     #     nextNodes = rightNodes
 
-    print("optimal nodes:", nextNodes)
+    print("dir prioritized nodes:", nextNodes)
     if len(nextNodes) == 0:
-        return "nextNodes list is empty!"
+        return "No optimal nodes!"
     else:
         # Always pick the node with the lowest index as the indexes are always put in order
-        # of the up, down, left, right rule
+        # of the up, down, left, right
         lowestIndex = None
         nextNode = None
+
+        # Put the first optimal node and its index as the initial lowest index
         for node in nextNodes:
             lowestIndex = node[5]
             nextNode = node
             break
+
+        # Find the node with the lowest index
         for node in nextNodes:
             if node[5] < lowestIndex:
                 lowestIndex = node[5]
                 nextNode = node
+        
         print("Next considered node:", nextNode)
         return nextNode
 
@@ -205,20 +212,21 @@ def ChooseNextConsideredNode(fringe, map):
 # Returns a string if it couldn't find a path
 def GraphSearch(search, size, start, end, map):
     # (row, col, Parent, Depth)
-    start = (start[0] - 1, start[1] - 1, "startParent", 0, "startDir", -1, "startParentDir")
+    start = (start[0] - 1, start[1] - 1, "startParent", 0, "startDir", -1)
     end = (end[0] - 1, end[1] - 1)
+    # parents = {}
     closed = {}
     fringe = [start]
     nodesConsidered = 1
     consideredNode = start
     fringeIndex = 0
 
-    while nodesConsidered <= 10000:
+    while nodesConsidered <= 8:
         print("Nodes considered:", nodesConsidered)
-        closed[(consideredNode[0], consideredNode[1])] = consideredNode[2]
+        # parents[consideredNode] = previousNode
         fringe.remove(consideredNode)
 
-        outMap, isEnd = CheckIfEndNode(consideredNode, start, end, map, closed)
+        outMap, isEnd = CheckIfEndNode(consideredNode, start, end, map)
         if isEnd:
             return outMap
 
@@ -249,14 +257,14 @@ else:
         # Removing the first unneeded space in each row
         print(printRow[1:])
 
-inpMap2 = [[1, 1, 1, 1, 1, 1, 4, 7, 8, "X"],
-        [1, 1, 1, 1, 1, 1, 1, 5, 8, 8],
-        [1, 1, 1, 1, 1, 1, 1, 4, 6, 7],
-        [1, 1, 1, 1, 1, "X", 1, 1, 1, 6],
-        [1, 1, 1, 1, 1, "X", 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [6, 1, 1, 1, 1, "X", 1, 1, 1, 1],
-        [7, 7, 1, "X", "X", "X", 1, 1, 1, 1],
-        [8, 115, 1, 1, 1, 1, 1, 1, 1, 1],
-        ["X", 8, 7, 1, 1, 1, 1, 1, 1, 1]]
+# inpMap2 = [[1, 1, 1, 1, 1, 1, 4, 7, 8, "X"],
+#         [1, 1, 1, 1, 1, 1, 1, 5, 8, 8],
+#         [1, 1, 1, 1, 1, 1, 1, 4, 6, 7],
+#         [1, 1, 1, 1, 1, "X", 1, 1, 1, 6],
+#         [1, 1, 1, 1, 1, "X", 1, 1, 1, 1],
+#         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#         [6, 1, 1, 1, 1, "X", 1, 1, 1, 1],
+#         [7, 7, 1, "X", "X", "X", 1, 1, 1, 1],
+#         [8, 115, 1, 1, 1, 1, 1, 1, 1, 1],
+#         ["X", 8, 7, 1, 1, 1, 1, 1, 1, 1]]
 # print(inpMap2[8][1])
