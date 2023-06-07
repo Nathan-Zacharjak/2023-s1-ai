3a76ba5eb6de21c97b0a5eedf73eff944d3c085c
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2023-06-07 17:01:15 +0930

    Added comment

--- viterbi.py
+++ viterbi.py
@@ -41,15 +41,7 @@ file.close()
 robotMap = np.array(robotMap, dtype=str)
 observations = np.array(observations, dtype=str)
 
+# Testing the values read for the input values
 output = [mapRows, mapCols, robotMap, noOfObservations, observations, errorRate]
 print(output)
 np.savez("output.npz", *output)
-
-
-# a = np.array([1, 2, 3])
-# b = np.array([4, 5, 6])
-# c = np.array([7, 8, 9])
-# maps = [a,b,c]
-
-# np.savez("output.npz", *maps)
-# print(maps)
