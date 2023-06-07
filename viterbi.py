2ab661569b0f6af38d70a02dd283d9b502ebc38f
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2023-06-07 15:47:07 +0930

    Added test output file

--- viterbi.py
+++ viterbi.py
@@ -1,4 +1,8 @@
 import numpy as np
 a = np.array([1, 2, 3])
-print("Hello!")
-print(a)
+b = np.array([4, 5, 6])
+c = np.array([7, 8, 9])
+maps = [a,b,c]
+
+np.savez("output.npz", *maps)
+print(maps)
